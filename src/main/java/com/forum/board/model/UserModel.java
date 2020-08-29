8b715ed0d57efb192a119f1086a93ea2e4af68a2
Breno <breno.cn@hotmail.com>
2020-08-29 16:36:20 -0300

    user_model adicionado

---
+++ src/main/java/com/forum/board/model/UserModel.java
@@ -0,0 +1,92 @@
+package com.forum.board.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user_model")
+public class UserModel {
+
+    private static final int USERNAME_LENGTH = 16;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true, length = USERNAME_LENGTH)
+    private String username;
+
+    @Column(name = "password", nullable = false, unique = true)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    public UserModel() {}
+
+    public UserModel(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return id.equals(userModel.id) &&
+                username.equals(userModel.username) &&
+                password.equals(userModel.password) &&
+                email.equals(userModel.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, email);
+    }
+
+}
