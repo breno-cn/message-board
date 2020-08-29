60d509bdb1967f5a6e67bc0f0fc92e79bc1c4dd8
Breno <breno.cn@hotmail.com>
2020-08-29 16:07:49 -0300

    init

---
+++ src/test/java/com/forum/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
