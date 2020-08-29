1b029de5b141670380997966f9a03f859b2c3a7e
Breno <breno.cn@hotmail.com>
2020-08-29 17:45:56 -0300

    BoardRepository alterado para RestRepository

--- src/main/java/com/forum/board/repository/BoardRepository.java
+++ src/main/java/com/forum/board/repository/BoardRepository.java
@@ -2,7 +2,14 @@ package com.forum.board.repository;
 
 import com.forum.board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {}
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "board", path = "board;")
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByName(@Param("name") String name);
+
+}
