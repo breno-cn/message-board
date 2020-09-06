e9955fcd60ae8c19e3fa983fb090eb19f0ae9684
Breno <breno.cn@hotmail.com>
2020-09-06 17:37:10 -0300

    CommentService adicionado

--- src/main/java/com/forum/board/controller/CommentController.java
+++ src/main/java/com/forum/board/controller/CommentController.java
@@ -1,71 +1,33 @@
 package com.forum.board.controller;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
-import com.forum.board.assembler.CommentAssembler;
-import com.forum.board.exception.CommentNotFoundException;
-import com.forum.board.exception.PostNotFoundException;
 import com.forum.board.model.Comment;
-import com.forum.board.model.Post;
-import com.forum.board.model.UserModel;
-import com.forum.board.repository.CommentRepository;
-import com.forum.board.repository.PostRepository;
-import com.forum.board.repository.UserRepository;
+import com.forum.board.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/comment")
 @Slf4j
 public class CommentController {
 
-    private final CommentRepository commentRepository;
+    private final CommentService commentService;
 
-    private final PostRepository postRepository;
-
-    private final UserRepository userRepository;
-
-    private final CommentAssembler commentAssembler;
-
-    public CommentController(CommentRepository commentRepository,
-                            PostRepository postRepository,
-                            UserRepository userRepository,
-                            CommentAssembler commentAssembler) {
-        this.commentRepository = commentRepository;
-        this.postRepository = postRepository;
-        this.userRepository = userRepository;
-        this.commentAssembler = commentAssembler;
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
     }
 
     @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getCommentById(@PathVariable(name = "id") Long id) {
-        Comment comment = commentRepository.findById(id)
-                .orElseThrow(() -> new CommentNotFoundException(id));
-
-        return ResponseEntity.ok(commentAssembler.toModel(comment));
+        return ResponseEntity.ok(commentService.findCommentById(id));
     }
 
     @GetMapping(value = "/post/{postId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getCommentsByPostId(@PathVariable(name = "postId") Long postId) {
-        List<EntityModel<Comment>> comments = commentRepository.findAllByPostId(postId)
-                .stream()
-                .map(commentAssembler::toModel)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(CollectionModel.of(
-                comments,
-                linkTo(methodOn(CommentController.class).getCommentsByPostId(postId)).withSelfRel()
-        ));
+        return  ResponseEntity.ok(commentService.findCommentsByPostId(postId));
     }
 
     @PostMapping(value = "/post/{postId}/new",
@@ -74,21 +36,9 @@ public class CommentController {
     public ResponseEntity<?> newCommentByPostId(@PathVariable(name = "postId") Long postId,
                                                 @RequestBody Comment comment,
                                                 Authentication authentication) throws RuntimeException {
-
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new PostNotFoundException(postId));
-
-        String username = authentication.getName();
-        UserModel userModel = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(username));
-
-        comment.setPost(post);
-        comment.setUser(userModel);
-        Comment saved = commentRepository.save(comment);
-
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(commentAssembler.toModel(saved));
+                .body(commentService.saveComment(postId, comment, authentication));
     }
 
 }
---
+++ src/main/java/com/forum/board/service/CommentService.java
@@ -0,0 +1,76 @@
+package com.forum.board.service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.forum.board.assembler.CommentAssembler;
+import com.forum.board.controller.CommentController;
+import com.forum.board.exception.CommentNotFoundException;
+import com.forum.board.exception.PostNotFoundException;
+import com.forum.board.model.Comment;
+import com.forum.board.model.Post;
+import com.forum.board.model.UserModel;
+import com.forum.board.repository.CommentRepository;
+import com.forum.board.repository.PostRepository;
+import com.forum.board.repository.UserRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final PostRepository postRepository;
+
+    private final UserRepository userRepository;
+
+    private final CommentAssembler commentAssembler;
+
+    public CommentService(CommentRepository commentRepository, CommentAssembler commentAssembler,
+                          PostRepository postRepository, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.commentAssembler = commentAssembler;
+    }
+
+    public EntityModel<Comment> findCommentById(Long id) {
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new CommentNotFoundException(id));
+
+        return commentAssembler.toModel(comment);
+    }
+
+    public CollectionModel<EntityModel<Comment>> findCommentsByPostId(Long id) {
+        List<EntityModel<Comment>> comments = commentRepository.findAllByPostId(id)
+                .stream()
+                .map(commentAssembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(
+                comments,
+                linkTo(methodOn(CommentController.class).getCommentsByPostId(id)).withSelfRel()
+        );
+    }
+
+    public EntityModel<Comment> saveComment(Long postId, Comment comment, Authentication authentication) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException(postId));
+
+        String username = authentication.getName();
+        UserModel user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        comment.setPost(post);
+        comment.setUser(user);
+
+        return commentAssembler.toModel(commentRepository.save(comment));
+    }
+
+}
